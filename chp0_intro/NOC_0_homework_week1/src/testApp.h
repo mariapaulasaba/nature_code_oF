b3fc5b73a5e63790c6994c2b9f73e186318c9cfb
mariapaulasaba <msd403@nyu.edu>
2013-01-31 01:08:43 -0500

    Gaussian Distribution

---
+++ chp0_intro/NOC_0_homework_week1/src/testApp.h
@@ -0,0 +1,22 @@
+
+
+#ifndef _SIMPLE_GAUSSIAN
+#define _SIMPLE_GAUSSIAN
+
+
+#include "ofMain.h"
+
+class testApp : public ofBaseApp{
+	
+public:
+    
+    void setup();
+    void draw();
+
+    
+};
+
+
+#endif
+
+
