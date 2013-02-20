d20ac50bb56b5b9f79abeda131edd4635df20249
mariapaulasaba <msd403@nyu.edu>
2013-02-20 11:33:03 -0500

    delete

--- chp2_forces/NOC_2_4_friction/src/mover.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  mover.cpp
-//  NOC_2_4_friction
-//
-//  Created by Maria Paula Saba dos Reis on 2/6/13.
-//
-//
-
-#include "mover.h"
--- chp3_oscillation/NOC_3__homework/src/Wave.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Wave.cpp
-//  NOC_3__homework
-//
-//  Created by Maria Paula Saba dos Reis on 2/19/13.
-//
-//
-
-#include "Wave.h"
